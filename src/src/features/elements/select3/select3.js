7ff31f8d022885d3123fc5a21f98485789b46349
Tsvetina Georgieva <tsvetinag@EM-BG-VPC025.europe-matrix.bg>
2016-03-21 18:10:20 +0200

    move selection up and down fix

--- src/src/features/elements/select3/select3.js
+++ src/src/features/elements/select3/select3.js
@@ -56,6 +56,8 @@ export class Select3 {
 
   bind() {
     this.opts = Object.assign(this.opts, this.options);
+    this.filteredDataShortStartIndex = 0;
+    this.filteredDataShortEndIndex = this.opts.visibleItemsCount - 1;
     this.itemsChanged();
   }
 
@@ -223,34 +225,15 @@ export class Select3 {
   }
 
   scrollToHoveredDatum() {
-    const halfCount = Math.floor(this.opts.visibleItemsCount / 2);
-    const fullCount = this.opts.visibleItemsCount;
     let hoveredDatumIndex = this.filteredData.indexOf(this.hoveredDatum);
-    let isInFirstHalfCount = hoveredDatumIndex < halfCount;
-    let isInLastHalfCount = hoveredDatumIndex > this.filteredData.length - 1 - halfCount;
-
-    let start, end;
-    if (this.filteredData.length <= this.opts.visibleItemsCount) {
-      // take all
-      start = 0;
-      end = this.filteredData.length - 1;
-    } else if (isInFirstHalfCount && !isInLastHalfCount) {
-      // take first fullCount
-      start = 0;
-      end = fullCount - 1;
-    } else if (!isInFirstHalfCount && isInLastHalfCount) {
-      // take last fullCount
-      end = this.filteredData.length - 1;
-      start = end - (fullCount - 1);
-    } else {// !isInFirstHalfCount && !isInLastHalfCount
-      //take halfCount before and halfCount after
-      start = hoveredDatumIndex - halfCount;
-      end = hoveredDatumIndex + halfCount;
+    if (hoveredDatumIndex < this.filteredDataShortStartIndex) {
+      this.filteredDataShortStartIndex = hoveredDatumIndex;
+      this.filteredDataShortEndIndex = this.filteredDataShortStartIndex + this.opts.visibleItemsCount - 1;
+    } else if (hoveredDatumIndex > this.filteredDataShortEndIndex) {
+      this.filteredDataShortEndIndex = hoveredDatumIndex;
+      this.filteredDataShortStartIndex = this.filteredDataShortEndIndex - (this.opts.visibleItemsCount - 1);
     }
-
-    this.filteredDataShortStartIndex = start;
-    this.filteredDataShortEndIndex = end;
-
+    
     this._refillFilteredDataShort();
   }
 
